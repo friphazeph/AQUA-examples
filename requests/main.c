f085f41b04e3eb84ed56669c3fa821f6c886796c
Obiwac <obiwac@gmail.com>
2018-10-12 23:19:19 +0200

    Updated requests example

--- requests/main.c
+++ requests/main.c
@@ -1,11 +1,12 @@
 
+#include "root.h"
 #include "lib/requests.h"
 
 var main(void) {
 	requests_t requests;
 	new_requests(&requests);
 	
-	chr* result = requests_get(&requests, "https://www.google.com/");
+	chr* result = requests_get(&requests, "https://www.test.com/");
 	
 	if (result) {
 		print("%s\n", result);
