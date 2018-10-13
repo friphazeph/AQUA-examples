1a13617bc77d1fb2f7c492bb32976fd0492c3291
Obiwac <obiwac@gmail.com>
2018-10-13 19:32:15 +0200

    Added warning message to server example

--- server/main.c
+++ server/main.c
@@ -2,6 +2,8 @@
 #include "root.h"
 #include "lib/string/strcmp.h"
 
+#define SERVER_PORT 0xA55
+
 var main(void) {
 	if (!socket_support()) {
 		print("WARNING Sockets are not supported on this platform\n");
@@ -11,30 +13,39 @@ var main(void) {
 		socket_t socket;
 		socket_server(&socket, nullptr, 0xA55);
 		
-		var pong_count = 0;
-		chr pong_message[SOCKET_DEFAULT_BUFFER_SIZE / sizeof(chr)];
-		
-		always {
-			chr* buffer = (chr*) socket_receive(&socket, SOCKET_DEFAULT_BUFFER_SIZE);
+		if (socket.error) {
+			print("WARNING Failed to create server\n");
+			return true;
 			
-			if (strcmp(buffer, "exit", SOCKET_DEFAULT_BUFFER_SIZE) == 0) {
-				print("Closing connection ...\n");
-				break;
+		} else {
+			print("Created server on port %lld\n", SERVER_PORT);
+			
+			var pong_count = 0;
+			chr pong_message[SOCKET_DEFAULT_BUFFER_SIZE / sizeof(chr)];
+			
+			always {
+				chr* buffer = (chr*) socket_receive(&socket, SOCKET_DEFAULT_BUFFER_SIZE);
 				
-			} elif (strcmp(buffer, "ping", SOCKET_DEFAULT_BUFFER_SIZE) == 0) {
-				sprintf((char*)                    pong_message, "pong %lld", pong_count++);
-				print("ping %s\n",                 pong_message);
-				socket_send(&socket, (const char*) pong_message, SOCKET_DEFAULT_BUFFER_SIZE);
-				
-			} else {
-				print("%s\n", buffer);
+				if (strcmp(buffer, "exit", SOCKET_DEFAULT_BUFFER_SIZE) == 0) {
+					print("Closing connection ...\n");
+					break;
+					
+				} elif (strcmp(buffer, "ping", SOCKET_DEFAULT_BUFFER_SIZE) == 0) {
+					sprintf((char*)                    pong_message, "pong %lld", pong_count++);
+					print("ping %s\n",                 pong_message);
+					socket_send(&socket, (const char*) pong_message, SOCKET_DEFAULT_BUFFER_SIZE);
+					
+				} else {
+					print("%s\n", buffer);
+					
+				}
 				
 			}
 			
+			socket_close(&socket);
+			return false;
+			
 		}
-		
-		socket_close(&socket);
-		return false;
 		
 	}
 	
